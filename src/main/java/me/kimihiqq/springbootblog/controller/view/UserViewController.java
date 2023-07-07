ca8876429d1e648ce6d359da5b914e87af0be408
kimihiqq <kimihiqq@gmail.com>
2023-07-07 15:47:20 +0900

    feat: view 컨트롤러 생성

---
+++ src/main/java/me/kimihiqq/springbootblog/controller/view/UserViewController.java
@@ -0,0 +1,15 @@
+package me.kimihiqq.springbootblog.controller.view;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserViewController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+
+}
